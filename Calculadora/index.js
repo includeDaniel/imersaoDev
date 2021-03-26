06a539247f30fa6068d82227c67a64d23ab8ded5
Daniel Nunes <danielzcomputacao@gmail.com>
2021-03-26 17:16:17 -0300

    Calculadora imersaoDev

---
+++ Calculadora/index.js
@@ -0,0 +1,27 @@
+
+
+let operacao = parseInt(prompt("Digite 1 para divisão, 2 para multiplicação, 3 para soma e 4 para subtração"))
+let primeiroValor = parseInt(prompt("Digite  primeiro valor:"))
+let segundoValor = parseInt(prompt("Digite o segundo valor"))
+
+  switch (operacao) {
+    case 1:
+      let resultadoDivisao = primeiroValor / segundoValor
+      alert(" " + primeiroValor + " / " + segundoValor  + " = " + resultadoDivisao + "  ")
+      break;
+    case 2:
+      let resultadoMultiplicacao = primeiroValor * segundoValor
+      alert(" " + primeiroValor + " x " + segundoValor  + " = " + resultadoMultiplicacao + "")
+      break;
+    case 3:
+      let resultadoSoma = primeiroValor + segundoValor
+      alert(" " + primeiroValor + " + " + segundoValor  + " = " + resultadoSoma + "") 
+      break;
+    case 4:
+      let resultadoSubtracao = primeiroValor - segundoValor
+      alert(" " + primeiroValor + " - " + segundoValor  + " = " + resultadoSubtracao + "")
+      break;
+    default:  
+      alert("Tente Novamente</h2>")
+  }
+
