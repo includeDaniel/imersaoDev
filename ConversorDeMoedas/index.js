984e3e7e7ed503492cedafeef42761645324cdb7
Daniel Nunes <danielzcomputacao@gmail.com>
2021-03-26 13:24:53 -0300

    Criei um botão para chamar o js

--- ConversorDeMoedas/index.js
+++ ConversorDeMoedas/index.js
@@ -1,8 +1,12 @@
-var valorEmDolarTexto = prompt("Qual o valor em dolar q voce quer converter?")
 
-var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+function ConvertendoMoeda () {
+    var valorEmDolarTexto = prompt("Qual o valor em dolar q voce quer converter?")
+    var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+    var valorEmReal = valorEmDolarNumero * 5.67
+    var valorEmRealFixado = valorEmReal.toFixed(2)
 
-var valorEmReal = valorEmDolarNumero * 5.67
-var valorEmRealFixado = valorEmReal.toFixed(2)
+    return(
+        alert(valorEmRealFixado)
+    )
 
-alert(valorEmRealFixado)
+}
