37017ec3a19f8d5bb3a69b1d365461e8ac4a79ba
Daniel Nunes <danielzcomputacao@gmail.com>
2021-03-30 12:53:12 -0300

    Tabela de classificação

---
+++ TabelaDeClassificação/index.js
@@ -0,0 +1,61 @@
+var paulo ={
+    nome: "Paulo",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+  }
+  var rafa = {
+    nome: "Rafa",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+  }
+  rafa.pontos = calculaPontos(rafa)
+  paulo.pontos = calculaPontos(paulo)
+  function calculaPontos(jogador) {
+    var pontos = (jogador.vitorias * 3) + jogador.empates 
+    return (
+      pontos
+    )
+  }
+  
+  var  jogadores =[rafa, paulo]
+  
+  exibirJogadoresNaTela(jogadores)
+  
+  function exibirJogadoresNaTela(jogadores) {
+    var html = ""
+    for(var i=0; i < jogadores.length; i++){
+      html +=  "<tr><td>" + jogadores[i].nome + "</td>"  
+      html +=  "<td>" + jogadores[i].vitorias + "</td>"  
+      html +=  "<td>" + jogadores[i].empates + "</td>"  
+      html +=  "<td>" + jogadores[i].derrotas + "</td>"  
+      html +=  "<td>" + jogadores[i].pontos + "</td>"
+      html += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+      html += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+      html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></tr>"
+    }
+    var  tabelaJogadores = document.getElementById('tabelaJogadores') 
+    tabelaJogadores.innerHTML = html
+   }
+  
+  function adicionarVitoria(i) {
+    var jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+  }
+  function adicionarEmpate(i) {
+    var jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+  }
+  function adicionarDerrota(i) {
+    var jogador = jogadores[i] 
+    jogador.derrotas++
+    exibirJogadoresNaTela(jogadores)
+  }
+
